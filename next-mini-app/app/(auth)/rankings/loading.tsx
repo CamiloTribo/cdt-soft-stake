5f657081de3acea7e8639d10ee195be724ec83dd
CamiloTribo <TRIBOcamilo@gmail.com>
2025-05-19 13:54:01 +0200

    carga ams rapida de las pages

--- next-mini-app/app/(auth)/rankings/loading.tsx
+++ next-mini-app/app/(auth)/rankings/loading.tsx
@@ -1,9 +1,3 @@
-import LoadingSpinner from "@/src/components/LoadingSpinner"
-
 export default function Loading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <LoadingSpinner size="lg" />
-    </div>
-  )
+  return null
 }
